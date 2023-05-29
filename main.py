b2a8bce5971bc5138b1ed9348f47ef8d1a520311
James Key <100178397+EternalBlue3@users.noreply.github.com>
2023-05-29 12:17:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from Keyboard import evaluate
 with open("Test.txt", "r") as fh:
     test = ''.join(fh.readlines()).replace('\n', '').lower()
     fh.close()
-    
+
+
 default = "qwertyuiopasdfghjkl;zxcvbnm,.?" # QWERTY
 minscore = math.inf
 best_keyboard = default
+iterations = 100000
 
-for keyboard in islice(permutations(default), 10000):
+for keyboard in islice(permutations(default), iterations):
     keyboard = ''.join(keyboard)
     keyboard = [keyboard[:10], keyboard[10:20], keyboard[20:30]]  # Modify keyboard for distance calculations
     score = evaluate(keyboard, test)
