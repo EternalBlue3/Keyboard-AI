0e1cc51ad69425f58cbb8f0464e7fd9dfa8b9b7b
James Key <100178397+EternalBlue3@users.noreply.github.com>
2023-05-29 23:15:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,19 @@ def evaluate_keyboard(keyboard):
     return evaluate(keyboard, test) # Return distance score
 
 default = "qwertyuiopasdfghjkl;zxcvbnm,.?"  # QWERTY
-iterations = 100000
+rstlne = "qwdfz;ukypaserlhniotgxcv?bjm,."
+best = "qwdfz;ukypaserlhniotxc.m?bv,jg"
+iterations = 1000000
 
 # Print statistics of QWERTY keyboard
 print("-"*45)
 print(f"QWERTY keyboard:\n    Keyboard: {[default[:10], default[10:20], default[20:30]]}\n    Score: {evaluate_keyboard(default)}")
 print("-"*45)
+print(f"RSTLNE keyboard:\n    Keyboard: {[rstlne[:10], rstlne[10:20], rstlne[20:30]]}\n    Score: {evaluate_keyboard(rstlne)}")
+print("-"*45)
+print(f"Best keyboard I have found thus far:\n    Keyboard: {[best[:10], best[10:20], best[20:30]]}\n    Score: {evaluate_keyboard(best)}")
+print("-"*45)
+
 
 if __name__ == '__main__':
     start = time.time()
@@ -29,7 +36,7 @@ if __name__ == '__main__':
     pool = Pool(processes=2)
 
     # Generate permutations up to the specified iterations
-    keyboard_permutations = islice(permutations(default), iterations)
+    keyboard_permutations = islice(permutations(rstlne), iterations)
 
     # Map the evaluate_keyboard function to the keyboard permutations using multiprocessing
     scores = pool.map(evaluate_keyboard, keyboard_permutations)
@@ -37,7 +44,7 @@ if __name__ == '__main__':
     # Find the minimum score and corresponding keyboard permutation
     minscore = min(scores)
     minscore_index = scores.index(minscore)
-    best_keyboard = ''.join(list(islice(permutations(default), iterations))[minscore_index])
+    best_keyboard = ''.join(list(islice(permutations(rstlne), iterations))[minscore_index])
     best_keyboard = [best_keyboard[:10], best_keyboard[10:20], best_keyboard[20:30]]
 
     print(f"Optimized keyboard:\n    Keyboard: {best_keyboard}\n    Score: {minscore}\n    Time to calculate: {time.time()-start}s")
